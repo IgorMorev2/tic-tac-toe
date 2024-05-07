d998e0e98f26e0f39c8e62b655b21904a08ee7d1
IgorMorev <igor.morev2000@gmail.com>
2024-05-07 16:29:59 +0300

    Finished Tic-Tac-Toe

---
+++ src/components/status/status.js
@@ -0,0 +1,7 @@
+export default function Status({ winner, xIsNext }) {
+  const status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
+  return <div className="status">{status}</div>;
+}
